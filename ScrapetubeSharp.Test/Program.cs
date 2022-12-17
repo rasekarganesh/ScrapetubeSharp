d3c8573d65e549c0cf44b0e13acb2d2f25dbc7d0
rasekarganesh <34262176+rasekarganesh@users.noreply.github.com>
2022-12-17 13:39:39 +0530

    updated JObject

--- ScrapetubeSharp.Test/Program.cs
+++ ScrapetubeSharp.Test/Program.cs
@@ -1,30 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ScrapetubeSharp.Test
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
             YouTube youTube = new YouTube();
-
-            var videos = youTube.Get_Channel("UCCezIgC97PvUuR4_gbFUs5g", null);
+            List<Newtonsoft.Json.Linq.JObject> videos;
+            videos = youTube.Get_Channel("UCCezIgC97PvUuR4_gbFUs5g", null);
             foreach (var item in videos)
             {
                 Console.WriteLine(item["videoId"].ToString());
             }
 
-            //var videos = youTube.Get_Playlist("PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU");
-            //foreach (var item in videos)
-            //{
-            //    Console.WriteLine(item["videoId"].ToString());
-            //}
+            videos = youTube.Get_Playlist("PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU");
+            foreach (var item in videos)
+            {
+                Console.WriteLine(item["videoId"].ToString());
+            }
 
-            //var videos = youTube.Get_Search("C#");
-            //foreach (var item in videos)
-            //{
-            //    Console.WriteLine(item["videoId"].ToString());
-            //}
+            videos = youTube.Get_Search("C#");
+            foreach (var item in videos)
+            {
+                Console.WriteLine(item["videoId"].ToString());
+            }
             Console.ReadLine();
         }
     }
